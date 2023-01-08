786e87ea2926dbb5b218586b6ad2a990c152a3f6
Raivis <highdoven@gmail.com>
2023-01-08 00:41:19 +0000

    remove key, add- channelCard, VideoCard, Videos, uddate index

--- src/components/index.js
+++ src/components/index.js
@@ -6,4 +6,5 @@ export { default as VideoDetail } from "./VideoDetail";
 export { default as SearchBar } from "./SearchBar";
 export { default as Sidebar } from "./Sidebar";
 export { default as Videos } from "./Videos";
-
+export { default as VideosCard } from "./VideoCard";
+export { default as ChannelCard } from "./ChannelCard";
